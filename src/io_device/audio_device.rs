f2720a3eb789a7154e1a697c46a91cf5ec3af4e7
冯翌航 <feng-yh23@mails.tsinghua.edu.cn>
2026-01-30 15:29:24 +0800

    fix: upsample original waveform

--- src/io_device/audio_device.rs
+++ src/io_device/audio_device.rs
@@ -1,16 +1,17 @@
 struct Channel {
     oscillator: web_sys::OscillatorNode,
-    waveform_raw: ([f32; Self::WAVEFORM_LENGTH], [f32; Self::WAVEFORM_LENGTH]),
+    waveform_raw: ([f32; Self::WAVEFORM_LENGTH * Self::UPSAMPLE_RATE], [f32; Self::WAVEFORM_LENGTH * Self::UPSAMPLE_RATE]),
 }
 
 impl Channel {
     const BIT_DEPTH: f32 = 15.0;
     const WAVEFORM_LENGTH: usize = 32;
+    const UPSAMPLE_RATE: usize = 2;
 
     fn new(context: &web_sys::AudioContext, constraints: &web_sys::PeriodicWaveConstraints) -> Self {
         let mut res = Self {
             oscillator: context.create_oscillator().unwrap(),
-            waveform_raw: ([0.0; Self::WAVEFORM_LENGTH], [0.0; Self::WAVEFORM_LENGTH]),
+            waveform_raw: ([0.0; Self::WAVEFORM_LENGTH * Self::UPSAMPLE_RATE], [0.0; Self::WAVEFORM_LENGTH * Self::UPSAMPLE_RATE]),
         };
         res.load_waveform(context, constraints);
         res.oscillator.connect_with_audio_node(&context.destination()).unwrap();
@@ -18,13 +19,14 @@ impl Channel {
         res
     }
     fn set_waveform_raw(&mut self, waveform: [f32; Self::WAVEFORM_LENGTH]) {
+        let waveform: [f32; Self::WAVEFORM_LENGTH * Self::UPSAMPLE_RATE] = std::array::from_fn(|i| {waveform[i / Self::UPSAMPLE_RATE]});
         const I: num::Complex::<f32> = num::Complex::<f32>::I;
         const PI: f32 = std::f32::consts::PI;
-        let mut waveform_complex = [num::Complex::<f32>::ZERO; Self::WAVEFORM_LENGTH];
+        let mut waveform_complex = [num::Complex::<f32>::ZERO; Self::WAVEFORM_LENGTH * Self::UPSAMPLE_RATE];
         // slow fourier transform
-        for i in 0..Self::WAVEFORM_LENGTH {
-            for j in 0..Self::WAVEFORM_LENGTH {
-                waveform_complex[i] += (-I * num::Complex::<f32>::from(2.0 * PI * i as f32 * j as f32 / Self::WAVEFORM_LENGTH as f32)).exp() * waveform[j];
+        for i in 0..Self::WAVEFORM_LENGTH * Self::UPSAMPLE_RATE {
+            for j in 0..Self::WAVEFORM_LENGTH * Self::UPSAMPLE_RATE {
+                waveform_complex[i] += (-I * num::Complex::<f32>::from(2.0 * PI * i as f32 * j as f32 / (Self::WAVEFORM_LENGTH * Self::UPSAMPLE_RATE) as f32)).exp() * waveform[j];
             }
         }
         
@@ -54,7 +56,6 @@ pub struct Speaker {
 
 impl Speaker {
     const CHANNEL_N: usize = 4;
-    const SQUARE: [u8; Channel::WAVEFORM_LENGTH] = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15];
     pub fn new(context: web_sys::AudioContext) -> Self {
         let constraint = web_sys::PeriodicWaveConstraints::new();
         constraint.set_disable_normalization(true);
@@ -69,12 +70,20 @@ impl Speaker {
         }
     }
     pub fn start(&mut self) {
-        self.channels[0].set_waveform(Self::SQUARE.clone(), 15);
-        //self.channels[0].waveform_raw.0[1] = 1.0;
+        let mut square = [0; Channel::WAVEFORM_LENGTH];
+        for i in 0..Channel::WAVEFORM_LENGTH {
+            square[i] = if i < Channel::WAVEFORM_LENGTH / 2 {15} else {0};
+        }
+        self.channels[0].set_waveform(square, 15);
         self.channels[0].load_waveform(&self.context, &self.constraint);
     }
     pub fn stop(&mut self) {
-        self.channels[0].set_waveform(Self::SQUARE.clone(), 0);
+        let mut square = [0; Channel::WAVEFORM_LENGTH];
+        for i in 0..Channel::WAVEFORM_LENGTH {
+            square[i] = if i < Channel::WAVEFORM_LENGTH / 2 {15} else {0};
+        }
+        self.channels[0].set_waveform(square, 0);
+        //self.channels[0].set_waveform(Self::SQUARE.clone(), 0);
         //self.channels[0].waveform_raw.0[1] = 0.0;
         self.channels[0].load_waveform(&self.context, &self.constraint);
     }
