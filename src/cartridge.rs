d8f73a01ce690fc4f3db9ce68e7701bd936d0574
冯翌航 <feng-yh23@mails.tsinghua.edu.cn>
2026-02-17 00:00:56 +0800

    feat: add peeks & pokes

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -1,1 +1,84 @@
 pub mod ram;
+use crate::cartridge::ram::{Vram, Ram};
+
+struct CartContext {
+    ram: Vec<Ram>,
+    active_bank: usize,
+}
+
+impl CartContext {
+    const BANK_N: usize = 8;
+    pub fn new() -> Self {
+        Self {
+            ram: vec![Ram::new(); Self::BANK_N],
+            active_bank: 0,
+        }
+    }
+
+    pub fn memcpy(&mut self, from: usize, to: usize, length: usize) {
+        let buffer: Vec<u8> = (from..from + length).into_iter().map(|x| self.ram[self.active_bank][x]).collect();
+        for i in 0..length {
+            self.ram[self.active_bank][to + i] = buffer[i];
+        }
+    }
+    pub fn memset(&mut self, addr: usize, value: u8, length: usize) {
+        for i in 0..length {
+            self.ram[self.active_bank][addr + i] = value;
+        }
+    }
+    pub fn peek_with_bits(&self, addr: usize, bits: usize) -> u8 {
+        (self.ram[self.active_bank][addr * bits / 8] >> (bits * (addr % (8 / bits)))) & ((1u16 << bits) - 1) as u8
+    }
+    pub fn peek(&self, addr: usize) -> u8 {
+        self.ram[self.active_bank][addr]
+    }
+    pub fn peek4(&self, addr: usize) -> u8 {
+        (self.ram[self.active_bank][addr / 2] >> (4 * (addr % 2))) & 0xf
+    }
+    pub fn peek2(&self, addr: usize) -> u8 {
+        (self.ram[self.active_bank][addr / 4] >> (2 * (addr % 4))) & 0b11
+    }
+    pub fn peek1(&self, addr: usize) -> u8 {
+        (self.ram[self.active_bank][addr / 8] >> (1 * (addr % 8))) & 0b1
+    }
+    pub fn poke_with_bits(&mut self, addr: usize, val: u8, bits: usize) {
+        let byte_borrow = &mut self.ram[self.active_bank][addr * bits / 8];
+        let bit_offset = ((addr % (8 / bits)) * bits) as u8;
+        let mask: u8 = ((((1u16 << bits) - 1) as u8) << bit_offset) ^ 0xff;
+        let val_mask = val << bit_offset;
+        *byte_borrow &= mask;
+        *byte_borrow |= val_mask;
+    }
+    pub fn poke(&mut self, addr: usize, val: u8) {
+        self.ram[self.active_bank][addr] = val;
+    }
+    pub fn poke4(&mut self, addr: usize, val: u8) {
+        self.poke_with_bits(addr, val, 4);
+    }
+    pub fn poke2(&mut self, addr: usize, val: u8) {
+        self.poke_with_bits(addr, val, 2);
+    }
+    pub fn poke1(&mut self, addr: usize, val: u8) {
+        self.poke_with_bits(addr, val, 1);
+    }
+
+    fn draw_while(&mut self, from: i32, to: i32, color: u32, coord: &dyn Fn(i32) -> (i32, i32)) {
+    }
+}
+#[cfg(test)]
+mod tests {
+    use crate::cartridge::CartContext;
+    use crate::cartridge::ram::{Vram, Ram};
+    #[test]
+    fn test_ram() {
+        let mut context = CartContext::new();
+        assert_eq!(Vram::BLIT_SEGMENT_OFFSET, 0x3ffc);
+        assert_eq!(Ram::GAMEPAD_MAPPING_OFFSET, 0x14e04);
+        //context.poke(0x12345, 0xab);
+        context.poke_with_bits(0x12345 * 2, 0xb, 4);
+        context.poke_with_bits(0x12345 * 2 + 1, 0xa, 4);
+        assert_eq!(context.peek4(0x12345 * 2), context.peek_with_bits(0x12345*2, 4));
+        assert_eq!(context.peek4(0x12345 * 2), 0xb);
+        assert_eq!(context.peek4(0x12345 * 2 + 1), 0xa);
+    }
+}
